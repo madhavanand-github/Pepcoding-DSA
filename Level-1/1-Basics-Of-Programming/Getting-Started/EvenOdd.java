07be01c00ac52cf40d948dee71b70836f2d27253
Madhav Anand <madhavanand402@gmail.com>
2021-11-02 16:43:23 +0530

    ➕ Even Odd

---
+++ Level-1/1-Basics-Of-Programming/Getting-Started/EvenOdd.java
@@ -0,0 +1,18 @@
+// File Name = Class Name
+public class EvenOdd {
+	public static void main(String[] args){
+
+		// Understand the flow of statements execution.
+		int x = 10;
+
+		if(x % 2 == 0){
+			System.out.println("EVEN");
+		}
+		else{
+			System.out.println("ODD");
+		}
+
+		System.out.println("I always execute");
+
+	}
+}
