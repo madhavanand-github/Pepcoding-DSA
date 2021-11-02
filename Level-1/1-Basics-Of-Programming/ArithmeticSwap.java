8ae064f5a79914294802f6376fede2febab393d4
Madhav Anand <madhavanand402@gmail.com>
2021-11-02 16:30:09 +0530

    ➕ Arithmetic Swap

---
+++ Level-1/1-Basics-Of-Programming/ArithmeticSwap.java
@@ -0,0 +1,13 @@
+
+public class ArithmeticSwap {
+	public static void main(String[] args){
+
+		int a = 10;
+		int b = 100;
+		a = a + b; // One have both
+		b = a - b; // You get what you don't subtract
+		a = a - b; 
+
+		System.out.println("a = " + a + "\nb = " + b);
+	}
+}
