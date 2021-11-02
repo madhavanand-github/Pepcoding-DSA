8ce284ac96512b8b343561fbfabc5401cf96f3bc
Madhav Anand <madhavanand402@gmail.com>
2021-11-02 16:06:49 +0530

    ➕ Print Z

---
+++ Level-1/1-Basics-Of-Programming/Getting-Started/PrintZ.java
@@ -0,0 +1,16 @@
+// File Name(PrintZ) = Class Name(PrintZ)
+public class PrintZ {
+
+	public static void main(String[] args){
+		
+		// Method 1
+		System.out.println("*****");
+		System.out.println("   * ");
+		System.out.println("  *  ");
+		System.out.println(" *   ");
+		System.out.println("*****");
+
+		// Method 2
+		System.out.print("*****\n   * \n  *  \n *   \n*****");
+	}
+}
