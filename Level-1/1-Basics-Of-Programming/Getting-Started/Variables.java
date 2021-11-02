0bba3951d5dfeb272fee0a0219a7d2dfbb23a7a6
Madhav Anand <madhavanand402@gmail.com>
2021-11-02 16:28:03 +0530

    ➕ Variables

---
+++ Level-1/1-Basics-Of-Programming/Getting-Started/Variables.java
@@ -0,0 +1,11 @@
+// File Name = Class Name
+public class Variables {
+
+	public static void name(String[] args) {
+
+		int age = 20;
+		String name = "Madhav Anand";
+		double money = 1334.2;
+		
+	}
+}
